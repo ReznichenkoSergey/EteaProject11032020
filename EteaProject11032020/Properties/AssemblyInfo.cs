48d061070caeb93369e1ecd4b11103c1a7fa5d8b
Сергей Резниченко <shift_2003@list.ru>
2020-03-12 08:21:03 +0200

    Rename

--- EteaProject11032020/Properties/AssemblyInfo.cs
+++ EteaProject11032020/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("EteaProject11032020")]
+[assembly: AssemblyTitle("ITEAProject11032020")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EteaProject11032020")]
+[assembly: AssemblyProduct("ITEAProject11032020")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
